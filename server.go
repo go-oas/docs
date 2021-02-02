da4c2414e6a3ed14bd1ed0408c91567ecaaedea2
Nesovic <nesovic@protonmail.com>
2021-02-02 20:01:39 +0100

    :white_check_mark: Added server tests. Improved testability of the server handler.

--- server.go
+++ server.go
@@ -1,11 +1,16 @@
 package docs
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"io/fs"
 	"log"
 	"net/http"
+	"os"
 	"strings"
+	"syscall"
+	"time"
 )
 
 const (
@@ -16,55 +21,166 @@ const (
 )
 
 type ConfigSwaggerUI struct {
-	Route, Port string
+	Route string
+	Port  string
+
+	httpServer *http.Server
+	initFS     FileSystem
+	stopper    chan os.Signal
 }
 
+// ServeSwaggerUI does what its name implies - runs Swagger UI on mentioned set port and route.
 func ServeSwaggerUI(conf *ConfigSwaggerUI) error {
 	if conf == nil {
 		return errors.New("swagger config is required")
 	}
 
-	route := conf.Route
-
-	if route == "" {
-		route = defaultRoute
+	if conf.Route == "" {
+		conf.Route = defaultRoute
 	}
 
-	fileServer := http.FileServer(FileSystem{http.Dir(defaultDirectory)})
-	http.Handle(route, http.StripPrefix(strings.TrimRight(route, fwSlashSuffix), fileServer))
+	if conf.initFS.isNil() {
+		conf.initFS = *initializeStandardFS()
+	}
+
+	if conf.httpServer == nil {
+		conf.initializeDefaultHTTPServer()
+	}
 
 	log.Printf("Serving SwaggerIU on HTTP port: %s\n", conf.Port)
+	conf.sigCont()
 
-	if err := http.ListenAndServe(fmt.Sprintf(":%s", conf.Port), nil); err != nil {
-		return fmt.Errorf("an error occurred while serving SwaggerUI: %w", err)
+	select {
+	case val := <-conf.stopper:
+		if shouldStop(val) {
+			err := conf.httpServer.Shutdown(context.Background())
+			if err != nil {
+				return fmt.Errorf("an error occurred while shutting down SwaggerUI: %w", err)
+			}
+		} else {
+			err := conf.httpServer.ListenAndServe()
+			if err != nil {
+				return fmt.Errorf("an error occurred while serving SwaggerUI: %w", err)
+			}
+		}
+
 	}
 
 	return nil
 }
 
+// FileSystem represents a wrapper for http.FileSystem, with relevant type func implementations.
 type FileSystem struct {
-	// fs is wrapped to avoid unwanted dir traversal.
-	fs http.FileSystem
+	fileSysInit http.FileSystem
+
+	fsOpenFn  fsOpenFn
+	getStatFn getStatFn
+	getIsDir  getIsDirFn
+}
+
+func initializeStandardFS() *FileSystem {
+	fsInit := http.Dir(defaultDirectory)
+
+	return &FileSystem{
+		fileSysInit: fsInit,
+		fsOpenFn:    newFSOpen(fsInit),
+		getStatFn:   newGetStatFn(),
+		getIsDir:    newGetIsDirFn(),
+	}
+}
+
+func (c *ConfigSwaggerUI) initializeDefaultHTTPServer() {
+	fileServer := http.FileServer(c.initFS)
+
+	c.httpServer = &http.Server{
+		Addr:    fmt.Sprintf(":%s", c.Port),
+		Handler: http.StripPrefix(strings.TrimRight(c.Route, fwSlashSuffix), fileServer),
+	}
+}
+
+func (c *ConfigSwaggerUI) sigCont() {
+	if c.stopper == nil {
+		var osSignal = make(chan os.Signal)
+		c.stopper = osSignal
+
+		go func() {
+			time.Sleep(20 * time.Millisecond)
+			osSignal <- syscall.SIGCONT
+		}()
+	}
+}
+
+func shouldStop(val os.Signal) bool {
+	if val == syscall.SIGINT || val == syscall.SIGKILL {
+		return true
+	}
+
+	return false
+}
+
+func newFSOpen(fis http.FileSystem) fsOpenFn {
+	return func(name string) (http.File, error) {
+		return fis.Open(name)
+	}
+}
+
+func newGetStatFn() getStatFn {
+	return func(file http.File) fileStatFn {
+		return func() (fs.FileInfo, error) {
+			return file.Stat()
+		}
+	}
+}
+
+func newGetIsDirFn() getIsDirFn {
+	return func(file fs.FileInfo) fsIsDirFn {
+		return func() bool {
+			return file.IsDir()
+		}
+	}
+}
+
+func (fis *FileSystem) isNil() bool {
+	if fis == nil {
+		return true
+	}
+
+	if fis.getStatFn == nil ||
+		fis.getIsDir == nil ||
+		fis.fsOpenFn == nil ||
+		fis.fileSysInit == nil {
+		return true
+	}
+
+	return false
 }
 
 // Open opens file. Returns http.File, and error if there is any.
-func (fs FileSystem) Open(path string) (http.File, error) {
-	f, err := fs.fs.Open(path)
+func (fis FileSystem) Open(path string) (http.File, error) {
+	f, err := fis.fsOpenFn(path)
 	if err != nil {
 		return nil, fmt.Errorf("failed to open file in path %s :%w", path, err)
 	}
 
-	fileInfo, err := f.Stat()
+	fileInfo, err := fis.getStatFn(f)()
 	if err != nil {
 		return f, fmt.Errorf("failed to fetch file info :%w", err)
 	}
 
-	if fileInfo.IsDir() {
+	if fis.getIsDir(fileInfo)() {
 		index := strings.TrimSuffix(path, fwSlashSuffix) + defaultIndexPath
-		if _, err = fs.fs.Open(index); err != nil {
+		if _, err = fis.fileSysInit.Open(index); err != nil {
 			return nil, fmt.Errorf("failed trimming path sufix :%w", err)
 		}
 	}
 
 	return f, nil
 }
+
+type (
+	fsOpenFn   func(name string) (http.File, error)
+	fsIsDirFn  func() bool
+	fileStatFn func() (fs.FileInfo, error)
+	getStatFn  func(file http.File) fileStatFn
+	getIsDirFn func(file fs.FileInfo) fsIsDirFn
+)
---
+++ server_test.go
@@ -0,0 +1,286 @@
+package docs
+
+import (
+	"errors"
+	"io/fs"
+	"math/rand"
+	"net/http"
+	"os"
+	"reflect"
+	"syscall"
+	"testing"
+	"testing/quick"
+	"time"
+)
+
+func TestUnitQuickInitializeStandardFS(t *testing.T) {
+	t.Parallel()
+
+	initStandardFS := func() bool {
+		fsInit := http.Dir(defaultDirectory)
+		want := FileSystem{
+			fileSysInit: fsInit,
+			fsOpenFn:    newFSOpen(fsInit),
+			getStatFn:   newGetStatFn(),
+			getIsDir:    newGetIsDirFn(),
+		}
+
+		got := *initializeStandardFS()
+
+		if got.fileSysInit != want.fileSysInit {
+			return false
+		}
+
+		if reflect.TypeOf(got.fsOpenFn) != reflect.TypeOf(want.fsOpenFn) {
+			return false
+		}
+
+		if reflect.TypeOf(got.getStatFn) != reflect.TypeOf(want.getStatFn) {
+			return false
+		}
+
+		if reflect.TypeOf(got.getIsDir) != reflect.TypeOf(want.getIsDir) {
+			return false
+		}
+
+		return true
+	}
+	if err := quick.Check(initStandardFS, nil); err != nil {
+		t.Errorf("Check failed: %#v", err)
+	}
+}
+
+func TestUnitQuickInitializeStandardFSErr(t *testing.T) {
+	t.Parallel()
+
+	config := quick.Config{
+		Values: func(values []reflect.Value, rand *rand.Rand) {
+			fis := http.Dir(defaultDirectory)
+			testFsOpenFn := newFSOpen(fis)
+			testGetStatFn := newGetStatFn()
+			testGetIsDirFn := newGetIsDirFn()
+
+			values[0] = reflect.ValueOf(testFsOpenFn)
+			values[1] = reflect.ValueOf(testGetStatFn)
+			values[2] = reflect.ValueOf(testGetIsDirFn)
+		},
+	}
+
+	initStandardFS := func(fsOpenFn fsOpenFn, getStatFn getStatFn, getIsDirFn getIsDirFn) bool {
+		fsInit := http.Dir(defaultDirectory)
+		want := FileSystem{
+			fileSysInit: fsInit,
+			fsOpenFn:    fsOpenFn,
+			getStatFn:   getStatFn,
+			getIsDir:    getIsDirFn,
+		}
+
+		got := *initializeStandardFS()
+
+		if got.fileSysInit != want.fileSysInit {
+			return false
+		}
+
+		if reflect.TypeOf(got.fsOpenFn) != reflect.TypeOf(want.fsOpenFn) {
+			return false
+		}
+
+		if reflect.TypeOf(got.getStatFn) != reflect.TypeOf(want.getStatFn) {
+			return false
+		}
+
+		if reflect.TypeOf(got.getIsDir) != reflect.TypeOf(want.getIsDir) {
+			return false
+		}
+
+		return true
+	}
+	if err := quick.Check(initStandardFS, &config); err != nil {
+		t.Errorf("Check failed: %#v", err)
+	}
+}
+
+func TestUnitNewFSOpen(t *testing.T) {
+	t.Parallel()
+
+	fsInit := http.Dir(defaultDirectory)
+
+	got := newFSOpen(fsInit)
+	want := func(name string) (http.File, error) {
+		return fsInit.Open(name)
+	}
+
+	gotRes, _ := got("/")
+	wantRes, _ := want("/")
+
+	gstat, _ := gotRes.Stat()
+	wstat, _ := wantRes.Stat()
+
+	if !reflect.DeepEqual(gstat, wstat) {
+		t.Error()
+	}
+}
+
+func TestUnitIsFSNil(t *testing.T) {
+	t.Parallel()
+
+	nilFS := (*FileSystem)(nil)
+	if !nilFS.isNil() {
+		t.Error()
+	}
+
+	fsInit := FileSystem{}
+	if !fsInit.isNil() {
+		t.Error()
+	}
+
+	fsStandard := *initializeStandardFS()
+	if fsStandard.isNil() {
+		t.Error()
+	}
+}
+
+func TestUnitFSOpen(t *testing.T) {
+	t.Parallel()
+
+	fsStd := *initializeStandardFS()
+	if fsStd.isNil() {
+		t.Error()
+	}
+
+	file, err := fsStd.Open("")
+	if err != nil {
+		t.Errorf("got an unexpected error: %v", err)
+	}
+
+	if file == nil {
+		t.Error("file is expected not to be nil")
+	}
+
+	fstat, _ := file.Stat()
+	if fstat.Name() != "dist" {
+		t.Errorf("got an unexpected file name: %v", fstat.Name())
+	}
+}
+
+func TestUnitFSOpenOpenErr(t *testing.T) {
+	t.Parallel()
+
+	fsInit := http.Dir(defaultDirectory)
+
+	fsStd := &FileSystem{
+		fileSysInit: fsInit,
+		fsOpenFn:    errFSOpen(t, fsInit),
+		getStatFn:   newGetStatFn(),
+		getIsDir:    newGetIsDirFn(),
+	}
+
+	if fsStd.isNil() {
+		t.Error()
+	}
+
+	_, err := fsStd.Open("")
+	if err == nil {
+		t.Error("expected an error, got none")
+	}
+}
+
+func TestUnitFSOpenGetStatErr(t *testing.T) {
+	t.Parallel()
+
+	fsInit := http.Dir(defaultDirectory)
+
+	fsStd := &FileSystem{
+		fileSysInit: fsInit,
+		fsOpenFn:    newFSOpen(fsInit),
+		getStatFn:   errGetStatFn(t),
+		getIsDir:    newGetIsDirFn(),
+	}
+
+	if fsStd.isNil() {
+		t.Error()
+	}
+
+	_, err := fsStd.Open("")
+	if err == nil {
+		t.Error("expected an error, got none")
+	}
+}
+
+func TestUnitFSOpenDirInnerOpenErr(t *testing.T) {
+	t.Parallel()
+
+	fsInitCorrect := http.Dir(defaultDirectory)
+	fsErr := http.Dir("!!\\@!")
+
+	fsStd := &FileSystem{
+		fileSysInit: fsErr,
+		fsOpenFn:    newFSOpen(fsInitCorrect),
+		getStatFn:   newGetStatFn(),
+		getIsDir:    statTrueGetIsDirFn(),
+	}
+
+	if fsStd.isNil() {
+		t.Error()
+	}
+
+	_, err := fsStd.Open("")
+	if err == nil {
+		t.Error("expected an error, got none")
+	}
+}
+
+func errFSOpen(t *testing.T, fis http.FileSystem) fsOpenFn {
+	t.Helper()
+
+	return func(name string) (http.File, error) {
+		file, _ := fis.Open(name)
+		return file, errors.New("triggerErr")
+	}
+}
+
+func errGetStatFn(t *testing.T) getStatFn {
+	t.Helper()
+
+	return func(file http.File) fileStatFn {
+		return func() (fs.FileInfo, error) {
+			fi, _ := file.Stat()
+
+			return fi, errors.New("triggerErr")
+		}
+	}
+}
+
+func statTrueGetIsDirFn() getIsDirFn {
+	return func(file fs.FileInfo) fsIsDirFn {
+		return func() bool {
+			return true
+		}
+	}
+}
+
+func TestUnitSwaggerUI(t *testing.T) {
+	conf := (*ConfigSwaggerUI)(nil)
+
+	err := ServeSwaggerUI(conf)
+	if err == nil {
+		t.Error("expected an error, got none")
+	}
+
+	var osSignal = make(chan os.Signal)
+
+	emptyRoute := &ConfigSwaggerUI{
+		Route:   "",
+		stopper: osSignal,
+	}
+
+	go func() {
+		time.Sleep(20 * time.Millisecond)
+		osSignal <- syscall.SIGINT
+	}()
+	_ = ServeSwaggerUI(emptyRoute)
+
+	if emptyRoute.Route != defaultRoute {
+		t.Errorf("route wasn't altered from its zero state to default route")
+	}
+}
